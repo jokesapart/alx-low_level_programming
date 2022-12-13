8ba703fade9fa93f7ea7aa38e8b43b3cf83fd25b
jokesapart <queebtfat@gmail.com>
2022-12-13 14:33:18 -0800

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,23 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDE
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(c);
+/**
+ * main.h file
+ * Header file that contains the declaration of our functions
+ */
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_last_sign(int n);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void timrs_table(void);
-int add(int a, int b);
+void times_table(void);
+int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-void num(int r, int n);
 
 #endif
